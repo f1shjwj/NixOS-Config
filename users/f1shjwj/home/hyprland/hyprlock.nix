7e260bb93cf21a3fd97a2f7c55ef5f4bbff6fca1
F1shjwj <jwjawa@outlook.com>
2025-05-02 12:07:03 +0800

    hyprlock

--- users/f1shjwj/home/hyprland/hyprlock.nix
+++ users/f1shjwj/home/hyprland/hyprlock.nix
@@ -25,7 +25,33 @@
     label = [
       {
         monitor = "";
-        text = "cmd[update:1000] echo $(date)";
+        text = "cmd[update:1000] echo \"<b><big> $(date +'%H %M') </big></b>\"";
+        font_size = 110;
+        shadow_passes = 2;
+        shadow_size = 1;
+        position = "0, 320";
+        halign = "center";
+        valign = "center";
+      }
+      {
+        monitor = "";
+        text = ''cmd[update:18000000] echo "<b><big> $(date +'%Y-%m-%d %A') </big></b>"'';
+        font_size = 25;
+        shadow_passes = 2;
+        shadow_size = 1;
+        position = "0, 200";
+        halign = "center";
+        valign = "center";
+      }
+      {
+        monitor = "";
+        text = "cmd[update:18000000] echo \"<b>Feels like<big> $(curl -s 'wttr.in?format=%t' | tr -d '+') </big></b>\"";
+        font_size = 18;
+        shadow_passes = 1;
+        shadow_size = 1;
+        position = "0, 40";
+        halign = "center";
+        valign = "bottom";
       }
     ];
   };
