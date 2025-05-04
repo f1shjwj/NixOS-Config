0f2064df8f20c873f8da1d866eb6369800f2a413
F1shjwj <jwjawa@outlook.com>
2025-05-04 15:55:19 +0800

    move modules

--- users/f1shjwj/modules.nix
+++ users/f1shjwj/modules.nix
@@ -1,19 +1,19 @@
 {
   imports = [
-    ../../modules/desktop/hyprland.nix
-    ../../modules/desktop/niri.nix
-    ../../modules/desktop/gamescope.nix
+    ./modules/desktop/hyprland.nix
+    ./modules/desktop/niri.nix
+    ./modules/desktop/gamescope.nix
 
-    ../../modules/virtual/docker.nix
-    # ../../modules/virtual/waydroid.nix
-    ../../modules/virtual/virtualbox.nix
+    ./modules/virtual/docker.nix
+    # ./modules/virtual/waydroid.nix
+    ./modules/virtual/virtualbox.nix
 
-    ../../modules/greetd.nix
-    ../../modules/fcitx5.nix
-    ../../modules/fonts.nix
-    ../../modules/power-profiles-daemon.nix
-    ../../modules/flatpak.nix
-    ../../modules/steam.nix
-    ../../modules/thunar.nix
+    ./modules/greetd.nix
+    ./modules/fcitx5.nix
+    ./modules/fonts.nix
+    ./modules/power-profiles-daemon.nix
+    ./modules/flatpak.nix
+    ./modules/steam.nix
+    ./modules/thunar.nix
   ];
 }
