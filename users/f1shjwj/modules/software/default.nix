edb9a14c23eb17c213b5f7dbc1c1389b0aea873b
F1shjwj <jwjawa@outlook.com>
2025-07-15 12:28:41 +0800

    250715

--- users/f1shjwj/modules/software/default.nix
+++ users/f1shjwj/modules/software/default.nix
@@ -1,55 +1,71 @@
-{ username, pkgs, ... }:
+{
+  username,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 {
   users.users.${username}.packages =
-    with pkgs;
-    [
-      neovim # TODO
-      ueberzugpp
-      openssl
-      nix-tree
-      you-get
-      shell-gpt # TODO: config
-      fzf
-      wev
-      tty-clock
-    ]
-    ++ [
-      (vscode.override {
-        commandLineArgs = "--enable-wayland-ime=true --wayland-text-input-version=3";
-      }).fhs
-      (code-cursor.override {
-        commandLineArgs = "--enable-wayland-ime=true --wayland-text-input-version=3";
-      })
-      gnumake
-      nixfmt-rfc-style
-      nixd
-      nix-search-tv
-      cachix
-      # beekeeper-studio
+    (
+      with pkgs;
+      [
+        neovim # TODO
+        ueberzugpp
+        openssl
+        nix-tree
+        you-get
+        shell-gpt # TODO: config
+        fzf
+        wev
+        tty-clock
+        ffmpeg
+        pandoc
+        imagemagick
+      ]
+      ++ [
+        (vscode.override {
+          commandLineArgs = "--enable-wayland-ime=true --wayland-text-input-version=3";
+        }).fhs
+        (code-cursor.override {
+          commandLineArgs = "--enable-wayland-ime=true --wayland-text-input-version=3";
+        })
+        gnumake
+        nixfmt-rfc-style
+        nixd
+        nix-search-tv
+        cachix
+        # beekeeper-studio
+      ]
+      ++ [
+        firefox
+        mpv
+        feh
+        telegram-desktop
+        obsidian
+        foliate
+        keepassxc
+        follow
+        qbittorrent
+        xfce.mousepad
+        wpsoffice-cn
+        mission-center
+        gimp3
+        netease-cloud-music-gtk
+        rustdesk
+        peazip
+        feishu
+        resilio-sync
+        # ventoy
+      ]
+      ++ [
+        techmino
+        hmcl
+      ]
+    )
+    ++ (with pkgs-unstable; [
+      chatbox
       gemini-cli
-    ]
-    ++ [
-      firefox
-      mpv
-      feh
-      telegram-desktop
-      obsidian
-      foliate
-      keepassxc
-      follow
-      qbittorrent
-      xfce.mousepad
-      wpsoffice-cn
-      mission-center
-      gimp3
-      netease-cloud-music-gtk
-      rustdesk
-      peazip
-    ]
-    ++ [
-      techmino
-      hmcl
-    ];
+    ]);
 
   # https://nixos.wiki/wiki/Appimage
   programs.appimage = {
--- users/f1shjwj/modules/software/obs-studio.nix
+++ users/f1shjwj/modules/software/obs-studio.nix
@@ -1,17 +1,8 @@
-{ username, pkgs, ... }:
+{ username, ... }:
 {
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
-    package = (
-      pkgs.obs-studio.overrideAttrs (
-        _finalAttrs: _prevAttrs: {
-          patches = _prevAttrs.patches ++ [
-            ./Fix-virtual-camera-start-failure.patch
-          ];
-        }
-      )
-    );
   };
 
   users.groups.video.members = [ username ];
