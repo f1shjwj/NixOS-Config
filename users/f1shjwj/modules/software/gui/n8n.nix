4f2b57b8b1fee69fd33a08833feabe8e27183125
F1shjwj <jwjawa@outlook.com>
2025-10-21 19:27:57 +0800

    251021 fix

--- users/f1shjwj/modules/software/gui/n8n.nix
+++ users/f1shjwj/modules/software/gui/n8n.nix
@@ -1,3 +1,4 @@
+{ username, lib, ... }:
 {
   services.n8n = {
     enable = true;
@@ -5,4 +6,17 @@
   };
 
   systemd.services.n8n.environment.all_proxy = "http://127.0.0.1:7890";
+
+  systemd.services.cloudflared-tunnel-n8n-tunnel.serviceConfig = {
+    Restart = lib.mkForce "always";
+    RestartSec = 10;
+  };
+
+  services.cloudflared.tunnels."n8n-tunnel" = {
+    credentialsFile = "/home/${username}/.cloudflared/n8n-tunnel.json";
+    ingress = {
+      "n8n.483917.xyz" = "http://localhost:5678";
+    };
+    default = "http_status:404";
+  };
 }
