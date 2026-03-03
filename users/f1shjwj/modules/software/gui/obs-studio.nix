a222cfdbdd50543dcf145acb52f2216b6e8dbd9c
F1shjwj <jwjawa@outlook.com>
2026-03-03 22:02:32 +0800

    obs虚拟摄像头

--- users/f1shjwj/modules/software/gui/obs-studio.nix
+++ users/f1shjwj/modules/software/gui/obs-studio.nix
@@ -1,4 +1,4 @@
-{ username, ... }:
+{ username, config, ... }:
 {
   programs.obs-studio = {
     enable = true;
@@ -6,4 +6,12 @@
   };
 
   users.groups.video.members = [ username ];
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=10 card_label="OBS Virtual Camera" exclusive_caps=1
+  '';
 }
